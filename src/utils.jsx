f22c77aa343328627dfcc1765d3d3245461fea76
Arsalanbashir831 <arsalanbashir831@gmail.com>
2024-04-15 02:30:23 +0500

    url change

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,3 +1,4 @@
 export const BASE_URL="https://virtuhire-backend.onrender.com/api/v1"
-export const NLP_SERVER="https://e82c-34-148-18-60.ngrok-free.app"
-export const QA_SERVER="https://e82c-34-148-18-60.ngrok-free.app"
+export const NLP_SERVER="https://e806-34-125-11-105.ngrok-free.app"
+export const QA_SERVER="https://e806-34-125-11-105.ngrok-free.app"
+
