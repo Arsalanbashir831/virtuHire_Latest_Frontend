99e6a13875bed5af579e6f9914d85425a8932f78
Arsalanbashir831 <arsalanbashir831@gmail.com>
2024-04-16 03:22:27 +0500

    llm server domain

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,4 +1,4 @@
 export const BASE_URL="https://virtuhire-backend.onrender.com/api/v1"
-export const NLP_SERVER="https://317e-35-233-133-109.ngrok-free.app"
-export const QA_SERVER="https://317e-35-233-133-109.ngrok-free.app"
+export const NLP_SERVER="https://capable-parakeet-terminally.ngrok-free.app"
+export const QA_SERVER="https://capable-parakeet-terminally.ngrok-free.app"
 
